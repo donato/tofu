479dc2c0aa121755ab6fc3096b756903f44a5c30
Donato <venar303@gmail.com>
2020-07-22 23:50:33 -0400

    Enable sabtargets button

---
+++ src/plugins/sabtargets.js
@@ -0,0 +1,77 @@
+define([
+  'utils/gui',
+  'utils/koc_utils'
+],
+  function (GUI, Koc) {
+  
+    function getSabTargets() {
+      getLux('&a=getsabtargets',
+         (r) => {
+          $("#_sab_content").html(r.responseText);
+
+          var q = document.getElementsByName('_luxbot_targets_t');
+          for (i = 0; i < q.length; i++) {
+            q[i].addEventListener('click', function (e) {
+              GM_openInTab('https://www.kingsofchaos.com/attack.php?id=' + String(e.target.id).replace(/__/, '')) }, true);
+          }
+
+          document.getElementById('getTodaysSabs').value = "View Your Sabs";
+          document.getElementById('getTodaysSabs').addEventListener('click', getTodaysSabs, true);
+          document.getElementById('getTodaysSabs').removeEventListener('click', getSabTargets, false);
+
+        });  
+    }
+
+    function getTodaysSabs() {
+      getLux('&a=getTodaysSabs',
+        function (r) {
+          document.getElementById('_sab_content').innerHTML = r.responseText;
+          document.getElementById('getTodaysSabs').value = "View Sab List";
+          document.getElementById('getTodaysSabs').addEventListener('click', getSabTargets, true);
+          document.getElementById('getTodaysSabs').removeEventListener('click', getTodaysSabs, false);
+        });
+    }
+
+    return {
+      name: 'Sablist',
+      description: "Sab targets button added to sidebar",
+      defaultEnabled: true,
+
+      run: function () {
+        this.addSabTargetsButton();
+      },
+      
+      addSabTargetsButton: function () {
+        var $sabButton = $('<a>', { 'href': '#' }).append(
+          $("<img>", {
+            'onclick': 'return false;',
+            'class': 'tofu',
+            'id': 'sidebar_sabtargets',
+            // 'src': gmGetResourceURL("sidebar_sabtargets")
+            'src': 'http://donatoborrello.com/bot/button_koc_sabtargets.gif'
+          }));
+
+        $sabButton.click(() => this.sabTargetsButton());
+
+        var $leftBarRows = $("td.menu_cell> table> tbody > tr");
+        $leftBarRows.eq(2).after($("<tr>").append($sabButton));
+      },
+      
+      sabTargetsButton: function () {
+        var $html = $("<table>", {
+          'class': 'table_lines tofu',
+          'id': '_luxbot_targets_table',
+          'width': '100%',
+          'cellspacing': 0,
+          'cellpadding': 0,
+          'border': 0
+        })
+          .append('<tr><td id="getTodaysSabs" ><input type="button" value="View Your Sabs" /></td></tr>'
+            + '<tr><td id="_sab_content">Loading... Please wait...</td></tr>');
+
+          
+        GUI.displayHtml($html);
+        getSabTargets();
+      },
+    };
+  });
