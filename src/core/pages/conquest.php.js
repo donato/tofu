9514429a87fb062524cb3615540d79d34f1b7e45
Donato <venar303@gmail.com>
2020-10-10 17:36:49 -0400

    Remove conquest tomfoolery

--- src/core/pages/conquest.php.js
+++ src/core/pages/conquest.php.js
@@ -1,14 +1,5 @@
-define(['jquery'], function ($) {
+define([], function () {
   return {
-    run: function () {
-      function doConquest() {
-        $("tr:contains('Wizards')").last().find("input[type='submit']").click();
-      }
-
-      if ($("table.table_lines>tbody>tr").length > 10) {
-        $("table.table_lines>tbody>tr:eq(2)").before("<tr><td id='wCount'>Wizards (x0)</td><td align='right'>1,000,000,000</td><td align='center'><button style='width:90%' id='doCon'>Go on a conquest against Wizards!</button></td></tr>");
-        $("#doCon").click(doConquest);
-      }
-    }
+    run: function () {}
   }
 });
